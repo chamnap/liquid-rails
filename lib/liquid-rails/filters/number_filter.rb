1a5a843ae51662f56557d10277ca9e97ef5f6795
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-09-02 21:40:30 +0700

    add number_filter

---
+++ lib/liquid-rails/filters/number_filter.rb
@@ -0,0 +1,24 @@
+module Liquid
+  module Rails
+    module NumberFilter
+      delegate \
+                :number_to_phone,
+                :number_to_currency,
+                :number_to_percentage,
+                :number_with_delimiter,
+                :number_with_precision,
+                :number_to_human_size,
+                :number_to_human,
+
+                to: :h
+
+      private
+
+        def h
+          @h ||= @context.registers[:helper]
+        end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Liquid::Rails::NumberFilter)
