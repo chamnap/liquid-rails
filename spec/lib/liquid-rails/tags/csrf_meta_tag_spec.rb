fdac5b646086acf02a7813c4010c3eb379e84f9a
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-09-03 12:03:25 +0700

    add missing files agains :(

---
+++ spec/lib/liquid-rails/tags/csrf_meta_tag_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module Liquid
+  module Rails
+    describe CsrfMetaTag do
+      let(:context) { ::Liquid::Context.new({}, {}, { view: view }) }
+
+      it '#crsf_meta_tags' do
+        view.stub(:protect_against_forgery?).and_return(true)
+
+        result = Liquid::Template.parse('{% csrf_meta_tags %}').render(context)
+        expect(result).to eq(%|<meta content=\"authenticity_token\" name=\"csrf-param\" />\n<meta content=\"#{view.form_authenticity_token}\" name=\"csrf-token\" />|)
+      end
+    end
+  end
+end
