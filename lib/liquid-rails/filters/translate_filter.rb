a342654e51f656581e46b88b9b7241276aaa5da7
Tomasz Stachewicz <tomekrs@o2.pl>
2015-01-27 18:04:54 +0100

    translate filter support for I18n interpolation

--- lib/liquid-rails/filters/translate_filter.rb
+++ lib/liquid-rails/filters/translate_filter.rb
@@ -1,10 +1,11 @@
 module Liquid
   module Rails
     module TranslateFilter
-      def translate(key, locale = nil, scope = nil)
+      def translate(key, locale = nil, scope = nil, options = Hash.new)
         locale ||= ::I18n.locale.to_s
+        options.merge!({locale: locale, scope: scope})
 
-        @context.registers[:view].translate(key.to_s, locale: locale, scope: scope)
+        @context.registers[:view].translate(key.to_s, options.with_indifferent_access)
       end
     end
   end
