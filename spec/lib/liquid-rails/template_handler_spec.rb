d2dbce3d59d52e90c0c56e25c48d42259dfd57c2
Dan Kubb <dan.kubb@gmail.com>
2015-05-08 16:36:39 -0700

    Fix whitespace and formatting consistency in template handler and specs

--- spec/lib/liquid-rails/template_handler_spec.rb
+++ spec/lib/liquid-rails/template_handler_spec.rb
@@ -5,7 +5,7 @@ describe 'Request', type: :feature do
     it 'renders with liquid template' do
       visit '/'
 
-      expect(page.body).to eq 'Liquid on Rails'
+      expect(page.body).to eq('Liquid on Rails')
     end
 
     it 'sets content_type as html by default' do
@@ -19,7 +19,7 @@ describe 'Request', type: :feature do
     it 'renders with layout' do
       visit '/index_with_layout'
 
-      expect(page.body).to eq "Application Layout\nLiquid on Rails"
+      expect(page.body).to eq("Application Layout\nLiquid on Rails")
     end
   end
 
@@ -27,20 +27,20 @@ describe 'Request', type: :feature do
     it 'no full path for the current controller' do
       visit '/index_partial'
 
-      expect(page.body).to eq "Application Layout\nLiquid on Rails\n\nHome Partial\nShared Partial"
+      expect(page.body).to eq("Application Layout\nLiquid on Rails\n\nHome Partial\nShared Partial")
     end
 
     it 'full path' do
       visit '/index_partial_with_full_path'
 
-      expect(page.body).to eq "Application Layout\nLiquid on Rails\n\nHome Partial\nShared Partial"
+      expect(page.body).to eq("Application Layout\nLiquid on Rails\n\nHome Partial\nShared Partial")
     end
 
     it 'respects namespace of original template for partials path' do
       visit '/foospace/bar/index_partial'
+
       expect(page.body.strip).to eq("Foospace::BarController\n\nBar Partial")
     end
-
   end
 
   context 'render with filter' do
