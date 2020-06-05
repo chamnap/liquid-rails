841145d9d1510aac3b52be96c51591c6e1a0f9be
Mikol Faro <mikol.faro@gmail.com>
2020-06-05 10:59:28 +0200

    Fix broken tests in rails 5.0

--- lib/liquid-rails/filters/translate_filter.rb
+++ lib/liquid-rails/filters/translate_filter.rb
@@ -2,9 +2,11 @@ module Liquid
   module Rails
     module TranslateFilter
       def translate(key, options={})
-        options = { 'locale' => ::I18n.locale.to_s }.merge(options)
+        # Convert everything to symbols
+        options = options.inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+        options = { locale: ::I18n.locale.to_s }.merge(options)
 
-        @context.registers[:view].translate(key.to_s, options.with_indifferent_access)
+        @context.registers[:view].translate(key.to_s, options)
       end
       alias_method :t, :translate
     end
