5d77f0585347d0642564a95fbfdd3e23fe1c6cfb
Atinder Singh <atinder90.a@gmail.com>
2023-10-09 08:54:32 +0530

    update deps

--- lib/liquid-rails/template_handler.rb
+++ lib/liquid-rails/template_handler.rb
@@ -2,7 +2,7 @@ module Liquid
   module Rails
     class TemplateHandler
 
-      def self.call(template)
+      def self.call(template, _source=nil)
         "Liquid::Rails::TemplateHandler.new(self).render(#{template.source.inspect}, local_assigns)"
       end
 
--- lib/liquid-rails/version.rb
+++ lib/liquid-rails/version.rb
@@ -1,5 +1,5 @@
 module Liquid
   module Rails
-    VERSION = '0.2.0'
+    VERSION = '0.2.3'
   end
 end
